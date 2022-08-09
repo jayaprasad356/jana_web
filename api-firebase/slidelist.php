a854bf8c65d92dd6a60c38f7780576356f8e07ca
Divaharsha <divakarvan03@gmail.com>
2022-08-09 13:12:14 -0700

    slides listed

--- api-firebase/slidelist.php
+++ api-firebase/slidelist.php
@@ -17,6 +17,13 @@ $db->sql($sql);
 $res = $db->getResult();
 $num = $db->numRows($res);
 if ($num >= 1) {
+    foreach ($res as $row) {
+        $temp['id'] = $row['id'];
+        $temp['name'] = $row['name'];
+        $temp['image'] = DOMAIN_URL  .$row['image'];
+        $rows[] = $temp;
+        
+    }
     $response['success'] = true;
     $response['message'] = "Slides Listed Successfully";
     $response['data'] = $res;
